e25cba26f2688395af6f194d7ff3efb8c2673541
Rich Gomez <0xkione@gmail.com>
2015-12-09 20:22:49 -0800

    Adding initial file structure and source files

---
+++ client/src/services/post-service.js
@@ -0,0 +1,46 @@
+export class PostService {
+  posts = [];
+
+  // Public functions
+  getPosts() {
+    return this.posts;
+  }
+
+  addPost() {
+    let id = this.posts.length;
+
+    if (!this.title || this.title === '') {
+      return;
+    }
+
+    this.posts.push({ id: id, title: this.title, content: this.content, upvotes: 0, link: '#/post/' + id, comments: []});
+    this.title = '';
+    this.content = '';
+  }
+
+  upvote(obj) {
+    ++obj.upvotes;
+  }
+
+  downvote(obj) {
+    --obj.upvotes;
+
+    if (obj.upvotes < 0) {
+      obj.upvotes = 0;
+    }
+  }
+
+  getPost(id) {
+    let idx = this.posts.findIndex((post) => post.id === id);
+    return this.posts[idx];
+  }
+
+  addComment(post) {
+    if (!this.content || this.content === '') {
+      return;
+    }
+
+    post.comments.push({ id: post.comments.length, author: 'name', upvotes: 0, content: this.content });
+    this.content = '';
+  }
+}
